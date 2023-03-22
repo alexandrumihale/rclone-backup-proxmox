0413b68da10353891ed0b3f673c07e71d719fc27
Alexandru Mihale <alexandru.mihale@NCM63228.local>
2023-03-22 21:42:43 +0200

    Created basic structure

--- backup.sh
+++ backup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 original_backups="/pooldirectory/dump" # Set this to where your vzdump files are stored
-achive_place="/backups"
+archive_place="/backups"
 maxdays=7
 rclone_instance="gdrive:" # do not forget the : 
 date_now=$(date +'%d-%h-%Y')
 
-# find $original_backups -type f -mtime +$max_days -exec /bin/rm -f {} \;
-search=$(find -L $archive_place -type f -name '*.tar' | grep *.tar )
+# delete file older than maxdays
+echo "Deleting files..."
+find -L $archive_place -mtime $maxdays -name *.tar -exec rm -f {} \;
 
 
+echo "Archiving files"
+tar cfP $archive_place/backup${date_now}.tar /pooldirectory/dump/* .
 
-#tar cvfP /backups/backup${date_now}.tar /pooldirectory/dump/* .
-#rclone copy $archive_place/*.tar $rclone_instance
-
-
-
+echo "Deleting old backup"
 ## idea: delete if different date NO DRY RUN
-#rclone --dry-run --include "*.tar" delete $rclone_instance
+rclone --dry-run --include "*.tar" delete $rclone_instance
 
+echo "Cloning..."
+rclone --dry-run copy $archive_place/*.tar $rclone_instance
