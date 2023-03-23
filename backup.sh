5feb730a2a0c8fb05f772527b4cbfee6246bc055
Alexandru Mihale <alexandru.mihale@NCM63228.local>
2023-03-23 10:34:08 +0200

    Cosmetized

--- backup.sh
+++ backup.sh
@@ -6,17 +6,27 @@ maxdays=7
 rclone_instance="gdrive:" # do not forget the : 
 date_now=$(date +'%d-%h-%Y')
 
+
+i=1
+sp="/-\|"
+
 # delete file older than maxdays
 echo "Deleting files..."
 find -L $archive_place -mtime $maxdays -name *.tar -exec rm -f {} \;
 
 
 echo "Archiving files"
-tar cfP $archive_place/backup${date_now}.tar /pooldirectory/dump/* .
+cd $archive_place
+tar zcfP backup${date_now}.tar $original_backups/*
+echo "Archived!"
+
 
 echo "Deleting old backup"
-## idea: delete if different date NO DRY RUN
-rclone --dry-run --include "*.tar" delete $rclone_instance
+rclone --include "*.tar" delete $rclone_instance
+echo "Deleted!"
 
 echo "Cloning..."
-rclone --dry-run copy $archive_place/*.tar $rclone_instance
+rclone copy $archive_place/*.tar $rclone_instance
+echo "Cloning done!"
+
+
